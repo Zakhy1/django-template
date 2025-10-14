0c0fde7b53a9cbbbe28f5808e07ca723949425f1
zakhy <=>
2025-10-14 12:05:40 +0700

    change containerization

---
+++ app/project/gunicorn.conf.py
@@ -0,0 +1,23 @@
+# gunicorn.conf.py
+
+import multiprocessing
+
+# --- Базовые настройки ---
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# --- Таймауты ---
+timeout = 60
+graceful_timeout = 30
+keepalive = 5
+
+# --- Логи ---
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# --- Дополнительно ---
+# preload_app = True  # загружать приложение до форка (ускоряет старты)
+# max_requests = 1000  # перезапуск воркера после N запросов
+# max_requests_jitter = 50  # добавляет случайность к перезапускам
