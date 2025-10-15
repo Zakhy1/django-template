18cc2e841bfc99e8c47ebed0c049a5054488792b
zakhy <z.lyrshikov@cybercode.pro>
2025-10-15 08:45:12 +0700

    format file

--- app/project/gunicorn.conf.py
+++ app/project/gunicorn.conf.py
@@ -1,23 +1,21 @@
-# gunicorn.conf.py
-
 import multiprocessing
 
-# --- Базовые настройки ---
+# Базовые настройки
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 
-# --- Таймауты ---
+# Таймауты
 timeout = 60
 graceful_timeout = 30
 keepalive = 5
 
-# --- Логи ---
+# Логи
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
 
-# --- Дополнительно ---
+# Дополнительно
 # preload_app = True  # загружать приложение до форка (ускоряет старты)
 # max_requests = 1000  # перезапуск воркера после N запросов
 # max_requests_jitter = 50  # добавляет случайность к перезапускам
